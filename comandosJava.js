f07665334c7f1333abe0e2994606fc0d72708217
maminsant <91914015+maminsant@users.noreply.github.com>
2022-01-20 13:16:00 -0300

    Create comandosJava.js

---
+++ comandosJava.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", inserir)
+let tamanho = 2;
+
+        function Inser(){
+			div = document.createElement("div");
+			div.style.fontSize = `${tamanho}em`;
+			div.innerHTML = `<button onclick="AumenTitulo()">+</button>
+            <button onclick="DimiTitulo()">-</button>
+			`;
+			document.body.prepend(div)
+		}
+
+        function AtualizaTamanho() {
+            const h1 = document.querySelector("h1");
+            h1.style.fontSize = `${tamanho}em`;
+        }
+        function AumenTitulo() {
+			if (tamanho < 5) {
+				tamanho += 0.5;
+				atualizatamanho();
+			}
+        }
+        function DimiTitulo() {
+            if (tamanho > 1) {
+                tamanho -= 0.5;
+                atualizatamanho();
+            }
+        }
