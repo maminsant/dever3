47a86841c64548af194cdf40f4b7a7a04b801faa
maminsant <91914015+maminsant@users.noreply.github.com>
2022-01-20 13:21:46 -0300

    Update comandosJava.js

--- comandosJava.js
+++ comandosJava.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", inserir)
 let tamanho = 2;
 
-        function Inser(){
+        function inserir(){
 			div = document.createElement("div");
 			div.style.fontSize = `${tamanho}em`;
-			div.innerHTML = `<button onclick="AumenTitulo()">+</button>
-            <button onclick="DimiTitulo()">-</button>
+			div.innerHTML = `<button onclick="aumentatitulo()">+</button>
+            <button onclick="diminuititulo()">-</button>
 			`;
 			document.body.prepend(div)
 		}
 
-        function Atualizatamanho() {
+        function atualizatamanho() {
             const h1 = document.querySelector("h1");
             h1.style.fontSize = `${tamanho}em`;
         }
-        function AumenTitulo() {
+        function aumentatitulo() {
 			if (tamanho < 5) {
 				tamanho += 0.5;
 				atualizatamanho();
 			}
         }
-        function DimiTitulo() {
+        function diminuititulo() {
             if (tamanho > 1) {
                 tamanho -= 0.5;
                 atualizatamanho();
